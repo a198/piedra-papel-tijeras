19ffa6d31bf904f2f4e662651d861dd240632088
Andrea Caldera <andrea.calderahernandez4@gmail.com>
2019-06-20 20:36:38 -0500

    Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+/*const playerInput =document.querySelector("#player-input");
+const winner=document.querySelector(".winner");
+
+function guardarNombre(){
+  const options = ["piedra","papel","tijeras"];
+  let computerSelection =Math.floor(Math.random()*2.99);
+  computerSelection=options[computerSelection];
+
+const playerSelection=playerInput.value;
+
+console.log("jugador1: " ,nombre1,playerSelection);
+
+
+  if(player1Selection===playerSelection2){
+    winner.innerText="Empate";
+  }else if(playerSelection==="papel"&&computerSelection==="piedra"){
+    winner.innerText="Ganó el jugador"
+  }
+}*/
